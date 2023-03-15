4a6bcd2660ee46b92ab01ade9026651a5f07c8a6
Juan Fernando Aispuro <Juanfernand2004@hotmail.com>
2023-03-15 17:42:30 -0600

    Initial commit

---
+++ Primera leccion.py
@@ -0,0 +1,31 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print ("Hello World")
+Hello World
+print ("Hello World"); print ("Adios mundo")
+Hello World
+Adios mundo
+#Aqui iria un comentario
+mi_nombre="mi nombre es juan!"
+mi_nombre
+'mi nombre es juan!'
+mi_nombre="mi nombre es\
+Juan"
+mi_nombnre
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    mi_nombnre
+NameError: name 'mi_nombnre' is not defined. Did you mean: 'mi_nombre'?
+mi_nombre
+'mi nombre esJuan'
+a=0
+for i in range (5):
+    a+=1
+    print(a)
+
+    
+1
+2
+3
+4
+5
